735203497cdf22cb6ca5f8b56fe0caf2fbf34244
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-08-24 13:15:38 +0530

    player class down

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    # frame rate logic
+    clock = pygame.time.Clock()
+    dt = 0
+
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     ## Game Loop
 
@@ -16,7 +26,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(0 , 0 , 0 ,0 ))
+        player.draw(screen)
         pygame.display.flip()
+
+        
+        time_passed = clock.tick(60)
+        dt = time_passed/1000
 
 
 
